520c4c2ef87f366293735a0781e4ed2bc494a437
davidemvf <davidemv.fiore@gmail.com>
2019-06-28 16:37:22 +0200

    Completato

---
+++ password.js
@@ -0,0 +1,8 @@
+//password
+var nome, cognome, colorepreferito;
+
+nome = prompt("Nome");
+cognome = prompt("Cognome");
+colorepreferito = prompt("Colore preferito");
+
+document.writeln(nome + cognome + colorepreferito + "19");
